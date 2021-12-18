ecae5e0f62f1646d31d532aef62415a37b032610
Maxim Mitin <maximus405@gmail.com>
2021-12-18 22:40:01 +0300

    fetch Data from data.plist , check if data already loaded to App

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -22,6 +22,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var ratingLabel: UILabel!
     @IBOutlet weak var myChoiceImageView: UIImageView!
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getDataFromFile()
+        
+    }
+    
+    
     @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
         
     }
@@ -34,10 +42,52 @@ class ViewController: UIViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private func getDataFromFile() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
         
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already ?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else {return}
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else {return}
+        
+        for dictionary in dataArray {
+            guard let entity = NSEntityDescription.entity(forEntityName: "Car", in: context) else {return}
+            let car = NSManagedObject(entity: entity, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as? Double ?? 0
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as? Int16 ?? 0
+            car.myChoice = carDictionary["myChoice"] as? Bool ?? false
+            
+            guard let imageName = carDictionary["imageName"] as? String else {return}
+            let image = UIImage(named: imageName)
+            let imageData = image?.pngData()
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] else {return UIColor()}
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
     }
     
 }
